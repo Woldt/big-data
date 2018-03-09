40422ab8ef1ae49ecc0d806a7c23e4bf514b2589
Andreas Norstein <andrnors@stud.ntnu.no>
2018-03-09 09:09:06 +0100

    task_8 skeleton

---
+++ task_8.py
@@ -0,0 +1,31 @@
+import pyspark
+from pyspark import SparkConf, SparkContext
+from operator import add
+
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("My app")
+         .set("spark.executor.memory", "1g"))
+sc = SparkContext()
+
+
+logFile = "./data/geotweets.tsv"  # Should be some file on your system
+stopwords = "./data/stop_words.txt"  # Should be some file on your system
+
+file = sc.textFile(logFile)  # Entire file
+stopwordFile = sc.textFile(stopwords)  # Entire file
+sample_file = file.sample(False, 0.01, 5)  # Sample file, 10% of original file
+
+UTC_TIME = 0
+COUNTRY_NAME = 1
+COUNTRY_CODE = 2
+PLACE_TYPE = 3
+PLACE_NAME = 4
+LANGUAGE = 5
+USERNAME = 6
+USER_SCREEN_NAME = 7
+TIMEZONE_OFFSET = 8
+NUMBER_OF_FRIENDS = 9
+TWEET_TEXT = 10
+LATITUDE = 11
+LONGITUDE = 12
