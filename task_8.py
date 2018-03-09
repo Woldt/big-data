6aed2ce13da28c364a5123d11855994fcf506e1b
Woldt <thmwld@gmail.com>
2018-03-09 09:52:22 +0100

    counting

--- task_8.py
+++ task_8.py
@@ -19,19 +19,10 @@ stopwords = "./data/stop_words.txt"  # Should be some file on your system
 
 data_frame = spark.read.option("sep", "\t").csv(logFile).toDF('UTC_TIME', 'COUNTRY_NAME', 'COUNTRY_CODE', 'PLACE_TYPE', 'PLACE_NAME', 'LANGUAGE', 'USERNAME', 'USER_SCREEN_NAME', 'TIMEZONE_OFFSET', 'NUMBER_OF_FRIENDS', 'TWEET_TEXT', 'LATITUDE', 'LONGITUDE')
 # sample_file = file.sample(False, 0.01, 5)  # Sample file, 10% of original file
-data_frame.show()
+# data_frame.show()
+
+data_frame.createOrReplaceTempView("tweets")
 
 
-UTC_TIME = 0
-COUNTRY_NAME = 1
-COUNTRY_CODE = 2
-PLACE_TYPE = 3
-PLACE_NAME = 4
-LANGUAGE = 5
-USERNAME = 6
-USER_SCREEN_NAME = 7
-TIMEZONE_OFFSET = 8
-NUMBER_OF_FRIENDS = 9
-TWEET_TEXT = 10
-LATITUDE = 11
-LONGITUDE = 12
+def get_number_of_tweets():
+    return spark.sql("SELECT * FROM tweets").count()
