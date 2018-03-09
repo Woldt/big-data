097c516643a48f79d447b5a8351d90557c8f8d08
Woldt <thmwld@gmail.com>
2018-03-09 09:37:29 +0100

    dataframe with named columns

--- task_8.py
+++ task_8.py
@@ -17,7 +17,7 @@ spark = SparkSession.builder.getOrCreate()
 logFile = "./data/geotweets.tsv"  # Should be some file on your system
 stopwords = "./data/stop_words.txt"  # Should be some file on your system
 
-data_frame = spark.read.option("sep", "\t").csv(logFile)
+data_frame = spark.read.option("sep", "\t").csv(logFile).toDF('UTC_TIME', 'COUNTRY_NAME', 'COUNTRY_CODE', 'PLACE_TYPE', 'PLACE_NAME', 'LANGUAGE', 'USERNAME', 'USER_SCREEN_NAME', 'TIMEZONE_OFFSET', 'NUMBER_OF_FRIENDS', 'TWEET_TEXT', 'LATITUDE', 'LONGITUDE')
 # sample_file = file.sample(False, 0.01, 5)  # Sample file, 10% of original file
 data_frame.show()
 
